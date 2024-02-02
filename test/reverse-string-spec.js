86e72afe7b799cfd056ad14f54c2cd15e00b8967
William <wcathey1604@gmail.com>
2024-02-01 22:42:27 -0600

    fixed assertion error

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,13 +1,11 @@
 const chai = require("chai")
 const expect = chai.expect;
 const reverseString = require("../problems/reverse-string.js");
-let result;
-beforeEach(() => {
-     result = "fun"
-})
+
 describe("reverseString", function() {
     it("should reverse the string", function() {
-
+        let result = reverseString('fun');
         expect(result).to.equal("nuf");
     })
 })
+
