44e8b56901aaf062875b1e5adeab3d5b97e0962e
William <wcathey1604@gmail.com>
2024-02-01 22:01:29 -0600

    created spec file but contains assertion error

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,12 @@
+function reverseString(str) {
+  let word = '';
+  for(let i = str.length -1; i <= 0; i--){
+  let char = str[i];
+  word  += char;
+  }
+  return word;
+}
+reverseString('fun')
 module.exports = function reverseString(string) {
-  // Your code here 
+
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,13 @@
-// Your code here 
+const chai = require("chai")
+const expect = chai.expect;
+const reverseString = require("../problems/reverse-string.js");
+let result;
+beforeEach(() => {
+     result = "fun"
+})
+describe("reverseString", function() {
+    it("should reverse the string", function() {
+
+        expect(result).to.equal("nuf");
+    })
+})
