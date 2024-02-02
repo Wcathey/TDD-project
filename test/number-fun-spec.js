76c65c940b9d15250427a4a550954b0903b501d0
jason <jason.witzel@gmail.com>
2024-02-01 23:24:09 -0600

    phase1 and phase2 working

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,25 @@
-// Your code here 
+const chai = require('chai');
+const expect = chai.expect;
+const {returnsThree, reciprocal} = require('../problems/number-fun')
+let num = 8;
+
+
+describe('returnsThree', function() {
+    it('returns 3', function() {
+        const three = 3
+        expect(three).to.equal(3)
+    })
+
+})
+
+
+
+describe('reciprocal', function() {
+    it('should intake a number', function() {
+        expect(num).assert.isNotNan
+    })
+    it('should return reciprocal of number', function() {
+        let result = reciprocal(num)
+        expect(result).to.equal(1/num)
+    })
+})
